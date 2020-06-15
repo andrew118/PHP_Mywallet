c264de1371328731a06e94ebe06758d93f0a79a3
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-06-15 13:21:39 +0200

    DB query tested - ok

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 
 	require_once "connect.php";
 	
-	$db_connection = new mysqli($host, $db_user, $db_password, $db_name);
+	$db_connection = @new mysqli($host, $db_user, $db_password, $db_name);
 	
 	if ($db_connection->connect_errno != 0)
 	{
@@ -13,9 +13,25 @@
 		$login = $_POST['login'];
 		$password = $_POST['password'];
 		
-		echo $login.' '.$password;
+		$sql = "SELECT * FROM users WHERE username='$login' AND password='$password'";
 		
-		
+		if ($result = $db_connection->query($sql))
+		{
+			$result_count = $result->num_rows;
+			
+			if ($result_count > 0)
+			{
+				$row = $result->fetch_assoc();
+				
+				echo '1. '.$row['id'].' 2. '.$row['username'].' 3. '.$row['password'].' 4. '.$row['email'];
+				
+				$result->free();
+			}
+			else
+			{
+				
+			}
+		}
 		
 		$db_connection->close();
 	}
