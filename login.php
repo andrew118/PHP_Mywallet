869797062f7b35cad871aa791d5838576fc76448
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-06-19 11:59:55 +0200

    Error handling for user login

--- login.php
+++ login.php
@@ -9,41 +9,51 @@
 	}
 	
 	require_once "connect.php";
+	mysqli_report(MYSQLI_REPORT_STRICT);
 	
-	$db_connection = @new mysqli($host, $db_user, $db_password, $db_name);
+	try
+	{
+		$db_connection = @new mysqli($host, $db_user, $db_password, $db_name);
 	
-	if ($db_connection->connect_errno != 0)
-	{
-		echo "Błąd: ".$db_connection->connect_errno;
-	}
-	else
-	{
-		$login = $_POST['login'];
-		$password = $_POST['password'];
-		
-		$login = htmlentities($login, ENT_QUOTES, "UTF-8");
-		
-		if ($result = $db_connection->query(
-		sprintf("SELECT id, username, password FROM users WHERE BINARY username='%s'",
-		mysqli_real_escape_string($db_connection, $login))))
+		if ($db_connection->connect_errno != 0)
 		{
-			$result_count = $result->num_rows;
+			throw new Exception(mysqli_connect_errno);
+		}
+		else
+		{
+			$login = $_POST['login'];
+			$password = $_POST['password'];
 			
-			if ($result_count > 0)
+			$login = htmlentities($login, ENT_QUOTES, "UTF-8");
+			
+			if ($result = $db_connection->query(
+			sprintf("SELECT id, username, password FROM users WHERE BINARY username='%s'",
+			mysqli_real_escape_string($db_connection, $login))))
 			{
-				$row = $result->fetch_assoc();
 				
-				if (password_verify($password, $row['password']))
+				$result_count = $result->num_rows;
+				
+				if ($result_count > 0)
 				{
-					$_SESSION['is_user_logged'] = true;
-					$_SESSION['logged_user_id'] = $row['id'];
-					$_SESSION['user'] = $row['username'];
+					$row = $result->fetch_assoc();
 					
-					unset($_SESSION['error']);
-					
-					$result->free();
-					
-					header('Location: main.php');
+					if (password_verify($password, $row['password']))
+					{
+						$_SESSION['is_user_logged'] = true;
+						$_SESSION['logged_user_id'] = $row['id'];
+						$_SESSION['user'] = $row['username'];
+						
+						unset($_SESSION['error']);
+						
+						$result->free();
+						
+						header('Location: main.php');
+					}
+					else
+					{
+						$_SESSION['error'] = '<div class="alert alert-danger col-10 col-sm-8 col-md-6 col-lg-4 mx-auto text-center" role="alert">Niepoprawny login lub hasło</div>';
+						header('Location: index.php');
+					}
 				}
 				else
 				{
@@ -53,14 +63,16 @@
 			}
 			else
 			{
-				$_SESSION['error'] = '<div class="alert alert-danger col-10 col-sm-8 col-md-6 col-lg-4 mx-auto text-center" role="alert">Niepoprawny login lub hasło</div>';
-				header('Location: index.php');
+				throw new Exception($db_connection->error);
 			}
+			
+			$db_connection->close();
 		}
-		
-		$db_connection->close();
 	}
-
-	
+	catch(Exception $e)
+	{
+		echo 'Błąd serwera. Przepraszamy za niedogodności. Spróbuj ponownie później.';
+		echo 'Dev Info: '.$e;
+	}
 	
 ?>
